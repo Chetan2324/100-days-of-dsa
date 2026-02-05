0dfc8f93391b62202fbc3b163d49193e9b5be2c2
Chetan Sharma <151332210+Chetan2324@users.noreply.github.com>
2026-02-06 01:58:25 +0530

    Implement function to move zeroes in array

---
+++ arrays/move-zeroes.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void moveZeroes(vector<int>& nums) {
+    int slow = 0;
+
+    for (int fast = 0; fast < nums.size(); fast++) {
+        if (nums[fast] != 0) {
+            swap(nums[slow], nums[fast]);
+            slow++;
+        }
+    }
+}
