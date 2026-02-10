70381a21a2e5f5c453814d7350831f56fefdaf4f
Chetan Sharma <151332210+Chetan2324@users.noreply.github.com>
2026-02-10 10:53:42 +0530

    Create palindrome-linked-list.cpp

---
+++ linked-list/palindrome-linked-list.cpp
@@ -0,0 +1,43 @@
+// LeetCode 234 - Palindrome Linked List
+// https://leetcode.com/problems/palindrome-linked-list/
+
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        if (!head || !head->next) return true;
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        // find middle
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        // reverse second half
+        ListNode* prev = NULL;
+        ListNode* curr = slow;
+
+        while (curr) {
+            ListNode* nextNode = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+
+        // compare both halves
+        ListNode* first = head;
+        ListNode* second = prev;
+
+        while (second) {
+            if (first->val != second->val)
+                return false;
+
+            first = first->next;
+            second = second->next;
+        }
+
+        return true;
+    }
+};
