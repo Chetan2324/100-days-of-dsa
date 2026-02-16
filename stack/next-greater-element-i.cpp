4137b90ed75c566651d8877acc45aa2ba3e0e5fb
Chetan Sharma <151332210+Chetan2324@users.noreply.github.com>
2026-02-16 16:21:00 +0530

    Implement Next Greater Element I solution

---
+++ stack/next-greater-element-i.cpp
@@ -0,0 +1,39 @@
+// LeetCode 496 - Next Greater Element I
+// https://leetcode.com/problems/next-greater-element-i/
+
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& arr) {
+
+        int n = arr.size();
+        vector<int> ans(n, -1);
+        stack<int> st;
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            while (!st.empty() && st.top() <= arr[i]) {
+                st.pop();
+            }
+
+            if (!st.empty()) {
+                ans[i] = st.top();
+            }
+
+            st.push(arr[i]);
+        }
+
+        unordered_map<int, int> mp;
+
+        for (int i = 0; i < n; i++) {
+            mp[arr[i]] = ans[i];
+        }
+
+        vector<int> res;
+
+        for (int i = 0; i < nums1.size(); i++) {
+            res.push_back(mp[nums1[i]]);
+        }
+
+        return res;
+    }
+};
