4adcc840279de945abb85335ff69c5406950d871
Chetan Sharma <151332210+Chetan2324@users.noreply.github.com>
2026-02-20 12:21:06 +0530

    Create maximum-depth-of-binary-tree.cpp

---
+++ trees/maximum-depth-of-binary-tree.cpp
@@ -0,0 +1,10 @@
+// LeetCode 104 - Maximum Depth of Binary Tree
+// https://leetcode.com/problems/maximum-depth-of-binary-tree/
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        return root ? 1 + max(maxDepth(root->left),
+                              maxDepth(root->right)) : 0;
+    }
+};
