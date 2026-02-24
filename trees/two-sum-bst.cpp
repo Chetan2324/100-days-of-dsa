70eba0275bd8589edc2a0ba75b80a23c41afe87f
Chetan Sharma <151332210+Chetan2324@users.noreply.github.com>
2026-02-24 10:12:37 +0530

    Create two-sum-bst.cpp

---
+++ trees/two-sum-bst.cpp
@@ -0,0 +1,26 @@
+// LeetCode 653 - Two Sum IV (Input is BST)
+// https://leetcode.com/problems/two-sum-iv-input-is-a-bst/
+
+class Solution {
+public:
+
+    bool helper(TreeNode* root, unordered_set<int>& st, int k) {
+
+        if (root == NULL)
+            return false;
+
+        if (st.find(k - root->val) != st.end())
+            return true;
+
+        st.insert(root->val);
+
+        return helper(root->left, st, k) ||
+               helper(root->right, st, k);
+    }
+
+    bool findTarget(TreeNode* root, int k) {
+
+        unordered_set<int> st;
+        return helper(root, st, k);
+    }
+};
